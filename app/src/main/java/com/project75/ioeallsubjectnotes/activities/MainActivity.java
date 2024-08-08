ef796860f8dd4091c3c32de5aa51f88d76d1d1a1
suman-shah <sumanshah485@gmail.com>
2024-08-08 22:13:01 +0545

    activities, model and adater packages are created

--- app/src/main/java/com/project75/ioeallsubjectnotes/activities/MainActivity.java
+++ app/src/main/java/com/project75/ioeallsubjectnotes/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.project75.ioeallsubjectnotes;
+package com.project75.ioeallsubjectnotes.activities;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.project75.ioeallsubjectnotes.R;
 
 public class MainActivity extends AppCompatActivity {
 
