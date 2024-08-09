e5b3fa7cf9b27df5d7471afdb16bd812c91d817d
suman-shah <sumanshah485@gmail.com>
2024-08-09 06:48:17 +0545

    Model class design completed for parent and child

--- app/src/main/java/com/project75/ioeallsubjectnotes/model/Topics.java
+++ app/src/main/java/com/project75/ioeallsubjectnotes/model/Topics.java
@@ -1,4 +1,18 @@
 package com.project75.ioeallsubjectnotes.model;
 public class Topics {
     String topicName;
+    String fileName;
+
+    public Topics(String topicName, String fileName) {
+        this.topicName = topicName;
+        this.fileName = fileName;
+    }
+
+    public String getTopicName() {
+        return topicName;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
 }
