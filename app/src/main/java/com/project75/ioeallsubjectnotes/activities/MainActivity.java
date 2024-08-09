5d15da114f988550f12476f210abc2d003d1da94
suman-shah <sumanshah485@gmail.com>
2024-08-09 09:41:27 +0545

    expandable listview

--- app/src/main/java/com/project75/ioeallsubjectnotes/activities/MainActivity.java
+++ app/src/main/java/com/project75/ioeallsubjectnotes/activities/MainActivity.java
@@ -1,6 +1,7 @@
 package com.project75.ioeallsubjectnotes.activities;
 
 import android.os.Bundle;
+import android.widget.ExpandableListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,8 +10,18 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.project75.ioeallsubjectnotes.R;
+import com.project75.ioeallsubjectnotes.adater.CustomAdapter;
+import com.project75.ioeallsubjectnotes.model.Chapter;
+import com.project75.ioeallsubjectnotes.model.Topics;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    ExpandableListView expandableListView;
+    CustomAdapter customAdapter;
+    List<Chapter> chapterList;
+    List<Topics> topicsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +33,36 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        expandableListView=findViewById(R.id.expandableListView);
+
+        addData();
+        sendData();
+
+
+    }
+    void addData(){
+        chapterList=new ArrayList<>();
+        topicsList=new ArrayList<>();
+// chapter 1
+        topicsList.add(new Topics("Introduction 1",""));
+        topicsList.add(new Topics("Introduction 2",""));
+        topicsList.add(new Topics("Introduction 3",""));
+        topicsList.add(new Topics("Introduction 4",""));
+        chapterList.add(new Chapter("Chapter 1",topicsList));
+// chapter 2
+        topicsList=new ArrayList<>();
+        topicsList.add(new Topics("Introduction1 1",""));
+        topicsList.add(new Topics("Introduction1 2",""));
+        topicsList.add(new Topics("Introduction1 3",""));
+        topicsList.add(new Topics("Introduction1 4",""));
+        chapterList.add(new Chapter("Chapter 2",topicsList));
+
+
+        sendData();
+
+    }
+    void sendData(){
+        customAdapter=new CustomAdapter(chapterList,MainActivity.this);
+        expandableListView.setAdapter(customAdapter);
     }
 }
